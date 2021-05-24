27a3236a8bbb7c1237ff46516e2c2b866f86ef33
Chris Joo <chrisj1225@gmail.com>
2021-05-24 12:18:59 -0400

    Set up errors reducer for session

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session/session_reducer';
+import errors from './errors/errors_reducer';
 
 const RootReducer = combineReducers({
-  session
+  session,
+  errors
 })
 
 export default RootReducer;
