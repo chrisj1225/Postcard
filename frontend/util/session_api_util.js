23fdc5be937b01190806afe43f4fe42bec5cdf1d
Chris Joo <chrisj1225@gmail.com>
2021-05-24 11:04:39 -0400

    create setAuthToken fxn

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+  } else {
+    delete axios.default.headers.common['Authorization'];
+  }
+};
+
