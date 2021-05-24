b74dc144785169a9db4c3b2a5da0e56ecc9e7e2e
Daniel Cho <thedanielcho@gmail.com>
2021-05-24 10:43:57 -0400

    Set up express routing for Users

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -10,6 +13,9 @@ mongoose
 
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
