f5a2d55b6f2e8e99c0a2c22b73f91fd32cfa0112
Chris Joo <chrisj1225@gmail.com>
2021-05-24 13:10:58 -0400

    Create signup & login apiutil & actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,14 +1,52 @@
-import * as APIUtil from '../util/session_api_util';
+import * as SessionAPIUtil from '../util/session_api_util';
 import jwt_decode from 'jwt-decode';
 
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveUserSignIn = () => ({
+  type: RECEIVE_USER_SIGN_IN
+});
 
 export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT
-})
+});
+
+export const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => {
+  return SessionAPIUtil.signup(user)
+    .then(() => dispatch(receiveUserSignIn()),
+    err => dispatch(receiveErrors(err.response.data))
+    )
+};
+
+export const login = user => dispatch => {
+  return SessionApiUtil.login(user)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      SessionApiUtil.setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(receiveCurrentUser(decoded));
+    })
+    .catch(err => {
+      dispatch(receiveErrors(err.response.data));
+    })
+}
 
 export const logout = () => dispatch => {
   localStorage.removeItem('jwtToken');
-  APIUtil.setAuthToken(false);
+  SessionAPIUtil.setAuthToken(false);
   dispatch(logoutUser());
-}
+};
