24b2214ce9b6a82ba301679baca5d6f0c99423a1
Chris Joo <chrisj1225@gmail.com>
2021-05-24 13:13:55 -0400

    Add RECEIVE_CURRENT_USER & RECEIVE_USER_SIGN_IN to session reducer

--- frontend/reducers/session/session_reducer.js
+++ frontend/reducers/session/session_reducer.js
@@ -1,4 +1,6 @@
 import { 
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_SIGN_IN,
   RECEIVE_USER_LOGOUT
 } from '../../actions/session_actions';
 
@@ -9,6 +11,17 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
+      };
+    case RECEIVE_USER_SIGN_IN:
+      return {
+        ...state,
+        isSignedIn: true
+      }
     case RECEIVE_USER_LOGOUT: 
       return {
         isAuthenticated: false,
