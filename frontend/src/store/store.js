1ebcbc3d289e143cf77cbda53cbcd8f5ea59852d
Chris Joo <chrisj1225@gmail.com>
2021-05-26 18:19:08 -0400

    Comment out redux logger

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => {
   return createStore(
     rootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
+    composeWithDevTools(applyMiddleware(thunk/*, logger*/))
   );
 };
 
