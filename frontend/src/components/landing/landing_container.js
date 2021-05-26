e9515119c9dad7badef58305e868818d6f3771e6
fdiengott <freddiengott@gmail.com>
2021-05-26 15:41:27 -0400

    Add link to login page if a logged out user tries to create a trip

--- frontend/src/components/landing/landing_container.js
+++ frontend/src/components/landing/landing_container.js
@@ -1,15 +1,18 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { fetchAllTrips /* fetchFollowedTrips */ } from '../../actions/trip_actions';				//actions
+import { openModal } from '../../actions/ui_actions';
 import Landing from './landing';				//display component
 
 const mapStateToProps = (state) => ({
   trips: state.entities.trips,
+  loggedIn: state.session.isAuthenticated
   // currentUserId: state.session.id, 
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchAllTrips: () => dispatch(fetchAllTrips()),
+  openModal: (type) => dispatch(openModal(type)),
   // fetchFollowedTrips: (userId) => dispatch(fetchFollowedTrips(userId)),
 });
 
