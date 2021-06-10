e444acc2a7d80ae82ff64ff8d58fb54ad5548b69
Chris Joo <chrisj1225@gmail.com>
2021-06-10 15:27:12 -0400

    Update fetchUserTrips action & all reducers

--- frontend/src/reducers/entities/users_reducer.js
+++ frontend/src/reducers/entities/users_reducer.js
@@ -4,18 +4,21 @@
 // } from '../../actions/session_actions';
 
 // import { RECEIVE_USER } from '../../actions/follow_actions';
+import { RECEIVE_USER_TRIPS } from '../../actions/follow_actions';
 
-// const UsersReducer = (state = {}, action) => {
-//   switch (action.type) {
-//     case RECEIVE_USER:
-//       return Object.assign({}, state, {[action.user._id]: action.user})
-//     case RECEIVE_CURRENT_USER:
-//       return Object.assign({}, state, {[action.currentUser._id]: action.currentUser})
-//     // case RECEIVE_USER_LOGOUT: 
-//     //   return {}
-//     default:
-//       return state;
-//   }
-// }
+const UsersReducer = (state = {}, action) => {
+  switch (action.type) {
+    // case RECEIVE_USER:
+    //   return Object.assign({}, state, {[action.user._id]: action.user})
+    // case RECEIVE_CURRENT_USER:
+    //   return Object.assign({}, state, {[action.currentUser._id]: action.currentUser})
+    // // case RECEIVE_USER_LOGOUT: 
+    // //   return {}
+    case RECEIVE_USER_TRIPS:
+      return Object.assign({}, action.data.user)
+    default:
+      return state;
+  }
+}
 
-// export default UsersReducer;
+export default UsersReducer;
